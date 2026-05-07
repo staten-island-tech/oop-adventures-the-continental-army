a0aba6574f6685f937aa3d814a75973ab9a8f7f3
Sabribrina <sabrinal117@nycstudents.net>
2026-05-07 10:17:21 -0400

    5/7/26

--- mmmmmmmmm.py
+++ mmmmmmmmm.py
@@ -1,14 +1,23 @@
 class MC:
-    def __init__ (self, name, hunger = 100, health = 100, stamina = 100, protection = 20, living = True): #Hunger Health Stamina Shield/Armour
+    def __init__ (self, name, inventory, hunger = 100, health = 100, stamina = 100, protection = 0, money = 0, living = True): #Hunger Health Stamina Shield/Armor
         self.name = name
+        self.inventory = inventory
         self.hunger = hunger
         self.health = health
         self.stamina = stamina
         self.protection = protection
+        self.money = money
         self.living = living
 
-    def statDec (self):
+    def passiveStat (self):
         self.hunger -= 5
+        self.stamina += 5
+
+    def warnings (self):
+        if self.hunger <= 25:
+            print(f"{self.name} is starving.")
+        if self.health <= 25:
+            print(f"{self.name} is bleeding out.")
 
     def maxmin (self):
         if self.hunger > 100:
@@ -30,3 +39,32 @@ class MC:
             self.protection = 20
         elif self.protection < 0:
             self.protection = 0
+
+class SHOP:
+    def __init__ (self, sword = 75, potion = 40, food = 15, armor = 75, bowANDarrow = 50):
+        self.sword = sword
+        self.potion = potion
+        self.food = food
+        self.armor = armor
+        self.bowANDarrow = bowANDarrow
+
+    def buy (self):
+        self.__money -= self.armor
+
+
+print("copy that intro msg uhdehjdgfkl")
+name = input("What is your name?")
+User = MC(name)
+
+while User.living == True:
+    Userinput = input("uiuirtldkhl")
+    Userinput = Userinput.lower()
+    if '' in Userinput:
+        User.passiveStat()
+        User.maxmin()
+        User.warnings()
+        print(f"{User.hunger} hunger")
+        print(f"{User.health} health")
+        print(f"{User.stamina} stamina")
+        print(f"{User.money} money")
+        print(f"{User.protection} armor")
