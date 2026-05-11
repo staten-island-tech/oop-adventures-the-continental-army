c8b2b7210627a473adcd2505dec8db695e7cb5b3
Sabribrina <sabrinal117@nycstudents.net>
2026-05-11 10:10:33 -0400

    5/11/26

--- mmmmmmmmm.py
+++ mmmmmmmmm.py
@@ -1,5 +1,5 @@
 class MC:
-    def __init__ (self, name, inventory = None, hunger = 100, health = 100, stamina = 100, damage = 10, protection = 0, money = 500, living = True): #Hunger Health Stamina Shield/Armor
+    def __init__ (self, name, inventory = None, hunger = 100, health = 100, stamina = 100, damage = 10, protection = 0, living = True): #Hunger Health Stamina Shield/Armor
         self.name = name
         self.inventory = inventory
         self.hunger = hunger
@@ -7,7 +7,6 @@ class MC:
         self.stamina = stamina
         self.damage = damage
         self.protection = protection
-        self.money = money
         self.living = living
 
     def passiveStat (self):
@@ -16,9 +15,13 @@ class MC:
 
     def warnings (self):
         if self.hunger <= 25:
-            print(f"{self.name} is starving.")
+            print(f"You are starving.")
+        elif self.hunger <= 10:
+            print(f"Your insides are burning.")
         if self.health <= 25:
-            print(f"{self.name} is bleeding out.")
+            print(f"Your starting to bleed out.")
+        elif self.health <= 10:
+            print(f"You are dying.")
 
     def maxmin (self):
         if self.hunger > 100:
@@ -44,9 +47,10 @@ class MC:
         elif self.protection < 0:
             self.protection = 0
 
-class SHOP:
-    def __init__ (self, name2, sword = 75, potion = 40, food = 15, armor = 75, bowandarrow = 50):
+class SHOP(MC):
+    def __init__ (self, name2, money = 500, sword = 75, potion = 40, food = 15, armor = 75, bowandarrow = 50):
         self.name2 = name2
+        self.money = money
         self.sword = sword
         self.potion = potion
         self.food = food
@@ -61,32 +65,36 @@ class SHOP:
         self.bowandarrow += 1
 
     def buysword (self):
-        self.__money -= self.sword
-        self.damage += 920
+        self.money -= self.sword
+        self.__damage += 920
 
     def buypotion (self):
-        self.__money -= self.potion
+        self.money -= self.potion
+        self.__health += 62
 
     def buyfood (self):
-        self.__money -= self.food
+        self.money -= self.food
+        self.__hunger += 98
 
     def buyarmor (self):
-        self.__money -= self.armor
+        self.money -= self.armor
         self.protection += 16
 
     def buybowandarrow (self):
-        self.__money -= self.bowandarrow
+        self.money -= self.bowandarrow
+        self.__damage += 5000000
 
 
 print("You’re walking across the street trying to figure out how to pay your debts to the loan sharks when you walk into the street and get hit by a truck. You wake up to see you’re trapped in a black space with nobody there, then a message on a screen pops up and it says “Teleporting in 3..2..1”. ")
 name = input(f"What is your name?")
 User = MC(name)
 User2 = SHOP(name)
+print("You've found a chest by your foot... (+500 gold)")
 
 while User.living == True:
-    Userinput = input(f"What do you wanna do now little boy?")
+    Userinput = input(f"What do you wanna do now {User.name}?")
     Userinput = Userinput.lower()
-    if User.health == 0 or User.hunger == 0 or User.money <= -100:
+    if User.health == 0 or User.hunger == 0 or User2.money <= -100:
         User.living = False
     if 'shop' in Userinput:
         User2.inflation()
@@ -96,8 +104,16 @@ while User.living == True:
         print(f"{User.hunger} hunger")
         print(f"{User.health} health")
         print(f"{User.stamina} stamina")
-        print(f"{User.money} money")
+        print(f"{User2.money} gold")
         print(f"{User.protection} armor")
         Userinput2 = input(f"Would you like to purchase a sword for {User2.sword}, a potion for {User2.potion}, food for {User2.food}, armor for {User2.armor}, or a bow & arrow for {User2.bowandarrow}?")
         if 'buysword' in Userinput2:
             User2.buysword()
+        elif 'buypotion' in Userinput2:
+            User2.buypotion()
+        elif 'buyfood' in Userinput2:
+            User2.buyfood()
+        elif 'buyarmor' in Userinput2:
+            User2.buyarmor()
+        elif 'buybowandarrow' in Userinput2:
+            User2.buybowandarrow()
